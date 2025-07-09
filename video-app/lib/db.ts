4a72e020dea653819575c7bacc82eb4fc80e8687
Aatishsinghshekhawat <aatishsinghdbs@gmail.com>
2025-07-09 21:19:42 +0530

    updated Video App

--- video-app/lib/db.ts
+++ video-app/lib/db.ts
@@ -1,5 +1,6 @@
 import { error } from "console";
 import mongoose from "mongoose";
+import { buffer } from "stream/consumers";
 
 const MONGODB_URI = process.env.MONGODB_URI!
 
@@ -11,4 +12,32 @@ let cached = global.mongoose
 
 if(!cached){
    cached= global.mongoose = {conn:null, promise: null}
+}
+
+export async function connectToDatabase() {
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+        const opts = {
+            bufferCommands : true,
+            maxPoolSize: 10
+        };
+
+
+        mongoose
+        .connect(MONGODB_URI, opts )
+        .then(() => mongoose.connection)
+    }
+
+    try{
+        cached.conn = await cached.promise
+    } catch (error){
+         cached.promise = null;
+         throw error;   
+    }
+
+    return cached.conn;
+
 }
