b95f72b380d716b1318cb3683d03cb026b77f7fe
Aatishsinghshekhawat <aatishsinghdbs@gmail.com>
2025-07-10 23:11:24 +0530

    Next Auth

--- video-app/lib/auth.ts
+++ video-app/lib/auth.ts
@@ -1,1 +1,11 @@
-export const authOptions = {};
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+    ],
+};
